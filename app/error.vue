265b45339c409ce553d3a71a10b26cc388a82127
belosludceva <belosludceva@ittest-team.ru>
2025-08-20 09:49:39 +0400

    DZ-2: add routing

---
+++ app/error.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { UIButtonsButton } from "#components";
+import type { NuxtError } from "nuxt/app";
+
+const props = defineProps<{
+  error: NuxtError;
+}>();
+</script>
+
+<template>
+  <div class="error-wrapper">
+    <div class="error-container">
+      <div class="error-status">{{ props.error?.statusCode }}</div>
+      <div class="error-message">
+        Страница не найдена, вернитесь на главную страницу
+      </div>
+
+      <NuxtLink to="/">
+        <UIButtonsButton class="back-btn">На главную</UIButtonsButton>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.error-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.error-container {
+  font-family: Roboto, sans-serif;
+  color: #292929;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.error-status {
+  font-size: 120px;
+  font-weight: 500;
+}
+
+.error-message {
+  font-size: 16px;
+  line-height: 24px;
+  margin-bottom: 24px;
+}
+
+.back-btn {
+  width: 301px;
+}
+</style>
---
+++ app/pages/create-post.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Создание поста</div>
+</template>
